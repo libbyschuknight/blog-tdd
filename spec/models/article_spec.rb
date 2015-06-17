f63147767c4299517698132b70d8a7645011ad30
libbyschuknight <libbysk@gmail.com>
2015-06-18 09:36:11 +1200

    Refactor article tests to use a 'let'.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe Article, type: :model do
 
   describe "#translate_description" do
 
+    let(:article) { FactoryGirl.build(:article) }
+
     it "will translate the description to spanish if passed in 'spanish'" do
-      article = FactoryGirl.build(:article)
       allow(Translate).to receive(:text).and_return("Ola")
       allow(Logger).to receive(:log)
       expect(article.translate_description("spanish")).to eq("Ola")
     end
 
     it "will log the translation" do
-      article = FactoryGirl.build(:article, description: "Hello")
       allow(Translate).to receive(:text).and_return("Ola")
       expect(Logger).to receive(:log).with("Hello", "Ola")
       article.translate_description("spanish")
