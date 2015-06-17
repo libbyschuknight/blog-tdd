7bcc51058b080eb4083840b2daa3008c4b2735e7
Libby Schumacher-Knight <apprentice@StMac5-2.local>
2015-06-17 12:40:05 +1200

    trying to pass user view list of articles but not got syntax right for test

--- spec/features/user-can-view-list-articles_spec.rb
+++ spec/features/user-can-view-list-articles_spec.rb
@@ -8,8 +8,19 @@ feature "User can view a list of articles" do
 
   scenario "it shows a list of articles" do
     visit articles_path(@artciles)
-    # expect page to have a list of 10 titles
-    expect(page).to have_selector('class', 'title')
+    # expect page to have a list of 10 titles??
+    # or just test that last article is showing
+    # can you test for all to show?
+    # or would first and last be good, or would just having last be fine??
+
+    puts body
+
+    # expect(page).to have_selector("id", @articles.last.id)
+
+    expect(page).to have_content("id='#{@articles.last.id}'")
+
+
+
   end
 
 
