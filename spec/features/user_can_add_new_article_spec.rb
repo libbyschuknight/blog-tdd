49b1b100da3afbf7f0da35ef4e51091eae745486
libbyschuknight <libbysk@gmail.com>
2015-06-18 09:10:39 +1200

    Add new article test.

--- spec/features/user_can_add_new_article_spec.rb
+++ spec/features/user_can_add_new_article_spec.rb
@@ -6,25 +6,21 @@ RSpec.feature "User can add new article" do
 
   end
 
-  scenario "user can add title to form" do
+  scenario "user can add title & description to form and submit" do
     visit new_article_path
 
     fill_in "Title", with: "First new blog post"
-    fill_in "Description", with: "This is the content part of my first blog post. This is where the main part of the blog post is wrtitten"
-    click_button "Submit Article"
+    fill_in "Description", with: "This is the content part of my first blog post. This is where the main part of the blog post is written"
+
+    click_link("Submit Article")
 
     expect do
-      expect(page).to
+      expect(page).to have_content("First new blog post")
+      p page
+
 
     end
 
 
-
-
-
   end
-
-
-
-
 end
