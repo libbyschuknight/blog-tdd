ba6a2e468878b6885efd437ea876d1bd4477fa50
libbyschuknight <libbysk@gmail.com>
2015-06-18 11:18:36 +1200

    add comment model/resource, migration for comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616212933) do
+ActiveRecord::Schema.define(version: 20150617223510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,14 @@ ActiveRecord::Schema.define(version: 20150616212933) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "article_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
 end
--- spec/features/user_can_add_new_article_spec.rb
+++ spec/features/user_can_add_new_article_spec.rb
@@ -2,25 +2,33 @@ require 'rails_helper'
 
 RSpec.feature "User can add new article" do
 
-  background do
-
-  end
-
+# is this the a better, different way of testing that user can create a new article??
   scenario "user can add title & description to form and submit" do
     visit new_article_path
 
     fill_in "Title", with: "First new blog post"
-    fill_in "Description", with: "This is the content part of my first blog post. This is where the main part of the blog post is written"
+    fill_in "Description", with: "This is the content part of my first blog post. This is where the main part of the blog post is written."
 
     click_link("Submit Article")
 
     expect do
       expect(page).to have_content("First new blog post")
-      p page
-
-
+      expect(page).to have_content("This is the content part of my first blog post. This is where the main part of the blog post is written.")
     end
+  end
 
+  scenario "once user submits form they are redirected to article page" do
+    visit new_article_path
+
+    fill_in "Title", with: "First new blog post"
+    fill_in "Description", with: "This is the content part of my first blog post. This is where the main part of the blog post is written."
+
+    click_link("Submit Article")
+
+    expect do
+      expect(current_path).to eql(new_article_path)
+      expect(page.current_page).to have_content("First new blog post")
+    end
 
   end
 end
