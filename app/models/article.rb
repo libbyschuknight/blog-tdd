846f634d8d8d9caa0d788fd7e7f8ad2e008cf911
libbyschuknight <libbysk@gmail.com>
2015-06-18 09:34:00 +1200

    Example with stubs and mocks.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
+class Logger
+  def self.log(original, tranlated)
+    puts "Sending message..."
+    sleep(5)
+    puts "Logged"
+  end
+end
+
+class Translate
+  def self.text(content, language)
+    "You should not see this text."
+  end
+end
+
 class Article < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
@@ -7,4 +21,10 @@ class Article < ActiveRecord::Base
   # did have length starting at 30, but broke lots of other tests
   # revisit and fix other tests so can have length minimum of 30, so there has to be something in description
   validates :description, length: { in: 0..500 }
+
+  def translate_description(language)
+    translation = Translate.text(description, language)
+    Logger.log(description, translation)
+    translation
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#translate_description" do
+
+    it "will translate the description to spanish if passed in 'spanish'" do
+      article = FactoryGirl.build(:article)
+      allow(Translate).to receive(:text).and_return("Ola")
+      allow(Logger).to receive(:log)
+      expect(article.translate_description("spanish")).to eq("Ola")
+    end
+
+    it "will log the translation" do
+      article = FactoryGirl.build(:article, description: "Hello")
+      allow(Translate).to receive(:text).and_return("Ola")
+      expect(Logger).to receive(:log).with("Hello", "Ola")
+      article.translate_description("spanish")
+    end
+
+  end
+
 end
